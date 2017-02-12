331e2179d1a8f9b523bca6676233fdbc4290a15d
sharkbite1414 <sharkbite1414@gmail.com>
2017-02-12 21:30:18 +0000

    Fixed 02_calculator.rb

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,16 @@
-#write your code here
+# write your code here
+def add(x, y)
+  x + y
+end
+
+def subtract(x, y)
+  x - y
+end
+
+def sum(arr)
+  sum = 0
+  arr.each do |i|
+    sum += i
+  end
+  sum.to_i
+end
