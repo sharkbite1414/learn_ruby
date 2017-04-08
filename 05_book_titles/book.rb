bc8cb33818cdff80e30b963758a90728c8a92062
sharkbite1414 <sharkbite1414@gmail.com>
2017-04-08 19:39:05 +0100

    completed books.rb

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,29 @@
 class Book
-# write your code here
+  # write your code here
+  attr_reader :title
+
+  def title=(title)
+    @title = title.split(" ").each_with_index do |word, index|
+      if index == 0
+        word.capitalize!
+        next
+      end
+      case word
+      when "and"
+        next
+      when "in"
+        next
+      when "of"
+        next
+      when "the"
+        next
+      when "a"
+        next
+      when "an"
+        next
+      else
+        word.capitalize!
+      end
+    end.join(" ")
+  end
 end
