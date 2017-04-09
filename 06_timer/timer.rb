864a008ba7da7af8cd2edef5b2ae0b6df6cc1bff
sharkbite1414 <sharkbite1414@gmail.com>
2017-04-09 16:32:36 +0100

    Finished timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,14 @@
 class Timer
-  #write your code here
+  # write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    quotiant = @seconds / 3600
+    remainder = @seconds % 3600
+    sprintf("%02d:%02d:%02d", quotiant, remainder / 60, remainder % 60)
+  end
 end
